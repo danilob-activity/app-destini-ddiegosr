e6f747d009e7f903d353415a5fa90f89f4253437
Diego Rodrigues <ddiegosr@hotmail.com>
2019-05-29 11:47:06 -0300

    created story and answer variables. map between stories and answers completed. save and get story index from app instance completed.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.londonappbrewery.destini.models.Answer;
+import com.londonappbrewery.destini.models.Story;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -13,23 +16,66 @@ public class MainActivity extends AppCompatActivity {
     Button mAnswerTop;
     Button mAnswerBottom;
     // TODO: Declare as variaveis aqui:
+    Answer mT1StoryAns1 = new Answer(R.string.T1_Ans1);
+    Answer mT1StoryAns2 = new Answer(R.string.T1_Ans2);
+
+    Answer mT2StoryAns1 = new Answer(R.string.T2_Ans1);
+    Answer mT2StoryAns2 = new Answer(R.string.T2_Ans2);
+
+    Answer mT3StoryAns1 = new Answer(R.string.T3_Ans1);
+    Answer mT3StoryAns2 = new Answer(R.string.T3_Ans2);
+    Story[] mStories = new Story[] {
+            new Story(R.string.T1_Story, mT1StoryAns1, mT1StoryAns2),
+            new Story(R.string.T2_Story, mT2StoryAns1, mT2StoryAns2),
+            new Story(R.string.T3_Story, mT3StoryAns1, mT3StoryAns2),
+            new Story(R.string.T4_End),
+            new Story(R.string.T5_End),
+            new Story(R.string.T6_End),
+    };
 
     //indice corrente da historia
-    private Story mStorySelected;
+    private int mStoryIndex = 0;
+    Story mStorySelected;
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("storyIndex", mStoryIndex);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            mStoryIndex = savedInstanceState.getInt("storyIndex");
+        }
+
+        mStorySelected = mStories[mStoryIndex];
+
 
         //TODO: Faça o link do layout com a activity
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mAnswerTop = findViewById(R.id.buttonTop);
+        mAnswerBottom = findViewById(R.id.buttonBottom);
+
+        mStoryTextView.setText(mStorySelected.getStoryID());
+        mAnswerTop.setText(mStorySelected.getAnswerTop().getAnswerID());
+        mAnswerBottom.setText(mStorySelected.getAnswerBottom().getAnswerID());
 
         //TODO:faça o mapeamento da história
+        mT1StoryAns1.setChildStory(mStories[2]);
+        mT1StoryAns2.setChildStory(mStories[1]);
+
+        mT3StoryAns1.setChildStory(mStories[5]);
+        mT3StoryAns2.setChildStory(mStories[4]);
+
+        mT2StoryAns1.setChildStory(mStories[2]);
+        mT2StoryAns2.setChildStory(mStories[3]);
 
         // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
         // do botão setVisibility(View.GONE):
-
 
     }
 
